c179afd0d06e186df39fbd1196ad37a631aa017f
Jethro Guce <jethro.guce@achievewithoutborders.com>
2020-02-24 22:15:15 +0800

    Task: Make fields readonly

--- wegen_custom/models/project.py
+++ wegen_custom/models/project.py
@@ -10,13 +10,26 @@ _logger = logging.getLogger(__name__)
 class Wegen_Task(models.Model):
     _inherit = "project.task"
 
+    is_admin = fields.Boolean(compute='_is_admin')
     is_manager = fields.Boolean(compute='_is_manager')
     is_assignee = fields.Boolean(compute='_is_assignee')
     is_creator = fields.Boolean(compute='_is_creator')
+    is_allow_edit = fields.Boolean(default=True, compute='_is_allow_edit')
+
+    def _is_admin(self):
+        user_groups = [group.id for group in self.env.user.groups_id]
+        _logger.info(str(user_groups))
+        is_project_admin = 19 in user_groups
+        is_access_admin = 2 in user_groups
+        is_settings_admin = 3 in user_groups
+
+        if is_project_admin or is_access_admin or is_settings_admin:
+            self.is_admin = True
+        else:
+            self.is_admin = False
 
     def _is_manager(self):
-        for task in self:
-            self.is_manager = self.manager_id.id == self.env.uid
+        self.is_manager = self.manager_id.id == self.env.uid
 
     def _is_assignee(self):
         self.is_assignee = self.user_id.id == self.env.uid
@@ -28,3 +41,13 @@ class Wegen_Task(models.Model):
             is_creator = False
 
         self.is_creator = is_creator
+
+    def _is_allow_edit(self):
+        if self.id is None:
+            self.is_allow_edit = True
+            return
+
+        if self.is_admin or self.is_creator or self.is_assignee or self.is_manager:
+            self.is_allow_edit = True
+        else:
+            self.is_allow_edit = False
