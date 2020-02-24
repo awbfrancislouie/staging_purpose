7cd18d870c14829de5cd558781e2a59525db312b
Jethro Guce <jethro.guce@achievewithoutborders.com>
2020-02-24 20:25:22 +0800

    Add logging

--- wegen_custom/models/project.py
+++ wegen_custom/models/project.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
 
 class Wegen_Task(models.Model):
     _inherit = "project.task"
@@ -9,7 +14,9 @@ class Wegen_Task(models.Model):
 
     def _is_allow_user_edit(self):
         for task in self:
+            _logger.info()
             self.is_allowed_edit = False
             if self.user_id == self.env.uid or self.manager_id == self.env.uid:
                 self.is_allowed_edit = True
-
+            message = f'User: {self.env.uid} Assignee: {self.user_id} Manager: {self.manager_id}'
+            _logger.info(message)
