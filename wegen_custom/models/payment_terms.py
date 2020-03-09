24b5d5362cd26eb84a5f8ab33b69b15666c27942
francis louie <francis.delrosario@achievewithoutborders.com>
2020-03-09 18:42:59 +0800

    update

---
+++ wegen_custom/models/payment_terms.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+class Wegen_Payment_Terms(models.Model):
+    _inherit='account.payment.term'
+    
+    payment_term_type = fields.Selection([('outright','Outright'),('installment','Installment'),('none','None')],
+                                        default='none')
+    
