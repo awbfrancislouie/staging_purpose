2d577b1afdb8a791cda71a0db601ba6788d03202
francis louie <francis.delrosario@achievewithoutborders.com>
2020-03-30 13:07:00 +0800

    update

--- wegen_custom/__manifest__.py
+++ wegen_custom/__manifest__.py
@@ -24,6 +24,7 @@
 
     # always loaded
     'data': [
+        # 'security/ir.model.access.csv',
         'views/task.xml',
         'views/templates.xml',
         'views/payment_terms.xml',
--- wegen_custom/models/crm_lead.py
+++ wegen_custom/models/crm_lead.py
@@ -9,7 +9,7 @@ class Wegen_Lead(models.Model):
     _inherit = "crm.lead"
 
     def _create_lead_partner_data(self, name, is_company, parent_id=False):
-        """ Override default partner data removes the user_id"""
+        """ Override default partner data removes the user_id """
         _logger.info('Overriding Default Lead Partner Data')
         retval = super(Wegen_Lead, self)._create_lead_partner_data(name, is_company, parent_id)
         retval['user_id'] = None
