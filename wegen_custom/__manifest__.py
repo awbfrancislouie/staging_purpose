e8d66c2511f4ec580a0bf4111169b5481b5a718e
francis louie <francis.delrosario@achievewithoutborders.com>
2020-04-07 11:18:06 +0800

    update version custom module

--- wegen_custom/__manifest__.py
+++ wegen_custom/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Localization',
-    'version': '13.3.1',
+    'version': '13.3.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'project', 'sale_management'],
