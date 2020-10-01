f383baef4e695515852da2e6cc46bdeb0fc1738e
Jethro Guce <gucejethro@gmail.com>
2020-10-01 20:22:33 +0800

    [ADD] define account type

--- wegen_custom/models/customer.py
+++ wegen_custom/models/customer.py
@@ -14,3 +14,13 @@ class CustomerMarketType(models.Model):
 
     x_name = fields.Char('Name')
     test = fields.Char('Test')
+
+
+class CustomerAccountType(models.Model):
+    _name = 'x_customer_account_type'
+    _description = 'Customer Account Type'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    x_name = fields.Char('Name')
+    x_code = fields.Char('Code')
+    test = fields.Char('Test')
