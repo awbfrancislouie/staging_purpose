efd7abb68544fc5e4088ed683874d7a040c4c44b
Jethro Guce <gucejethro@gmail.com>
2020-10-01 20:10:33 +0800

    [ADD] wegen special

---
+++ wegen_special/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 
+{
+    'name': 'WeGen Special',
+    'version': '13.0.0.1',
+    'depends': [
+        'calendar',
+    ],
+    'external_dependencies': {},
+    'author': 'Jimuel Atienza',
+    'website': '',
+    'summary': """Special Modifications for WeGen""",
+    'description': """
+        Special Modifications for WeGen
+    """,
+    'category': 'Special',
+    'data': [
+        'data/ir_model_data_data.sql',
+        'views/calendar_event_views.xml',
+        'data/mail_template_data.xml',
+    ],
+    'qweb': [],
+    'css': [],
+    'images': [],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
