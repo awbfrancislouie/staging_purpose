094ea32ad565f6c2c50df0a3f8f66c37a7ba70da
Jethro Guce <gucejethro@gmail.com>
2020-10-12 20:44:32 +0800

    [FIX] Overrride test case

--- wegen_custom/tests/test_crm_ui.py
+++ wegen_custom/tests/test_crm_ui.py
@@ -4,7 +4,10 @@ from odoo.addons.crm.tests.test_crm_ui import TestUi
 
 
 @odoo.tests.tagged('post_install', '-at_install')
-class WeGenCustomProjectTestUi(TestUi):
+class WeGenCustomCrmTestUi(TestUi):
+
+    def setUp(self):
+        super(WeGenCustomCrmTestUi, self).setUp()
 
     def test_01_crm_tour(self):
         pass
--- wegen_custom/tests/test_project_ui.py
+++ wegen_custom/tests/test_project_ui.py
@@ -6,5 +6,8 @@ from odoo.addons.project.tests.test_project_ui import TestUi
 @odoo.tests.tagged('post_install', '-at_install')
 class WeGenCustomProjectTestUi(TestUi):
 
+    def setUp(self):
+        super(WeGenCustomProjectTestUi, self).setUp()
+
     def test_01_project_tour(self):
         pass
